d216605604a4aa3be802f4d56ad1278693c14d90
motobi5 <motobi@naver.com>
2022-10-30 14:32:39 +0900

    221030_TopBar_2차완료

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function megaMenu__init() {
     $(".bg-white").addClass("active");
     $(".topBar .inner .mega").addClass("on");
     $(".topBar .inner .mega .megaMenu01 > div").addClass("active");
-    $(".topBar .inner .mega  .megaMenu > div > .img-box").addClass("active");
     $(".topBar .inner .mega .megaMenu > ul").addClass("active");
     $(".topBar .inner .mega .megaMenu01").addClass("active");
   });
@@ -15,9 +14,67 @@ function megaMenu__init() {
     $(".topBar .inner .mega  .megaMenu").removeClass("active");
     $(".bg-white").addClass("active");
     $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu02 > div").addClass("active");
     $(".topBar .inner .mega .megaMenu > ul").addClass("active");
-    $(".topBar .inner .mega  .megaMenu02").addClass("active");
+    $(".topBar .inner .mega .megaMenu02").addClass("active");
   });
+  $(".menuList01 > li:nth-child(3)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu03 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu03").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(4)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu04 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu04").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(5)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu05 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu05").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(6)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu06 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu06").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(7)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu07 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu07").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(8)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu08 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu08").addClass("active");
+  });
+  $(".menuList01 > li:nth-child(9)").mouseenter(function(){
+    $(".topBar .inner .mega  .megaMenu").removeClass("active");
+    $(".bg-white").addClass("active");
+    $(".topBar .inner .mega").addClass("on");
+    $(".topBar .inner .mega .megaMenu09 > div").addClass("active");
+    $(".topBar .inner .mega .megaMenu > ul").addClass("active");
+    $(".topBar .inner .mega .megaMenu09").addClass("active");
+  });
+
   
   $(".topBar .inner .mega").mouseleave(function(){
     $(".topBar .inner .mega  .megaMenu").removeClass("active");
@@ -28,5 +85,5 @@ function megaMenu__init() {
 }
 
 megaMenu__init();
-// megaMenuUp__init();
+
 
